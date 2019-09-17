d03856c99f33c6782a43cbed9e8d215959c2f5f9
Eduardo Moura <esm7@cin.ufpe.br>
2019-09-16 22:07:50 -0300

    Add files via upload

---
+++ arrays.rs
@@ -0,0 +1,25 @@
+use std::mem;
+
+pub fn run() {
+
+    let mut numbers: [i32; 4] = [1, 2, 3, 4];
+
+    // Reassingn value
+    numbers[2] = 20;
+
+    println!("{:?}", numbers);
+
+    // Get single val
+    println!("Single value: {}", numbers[0]);
+
+    // Get array length
+    println!("Array length: {}", numbers.len());
+
+    // Arrays are stack allocated
+    println!("Array occupies {} bytes", mem::size_of_val(&numbers));
+
+    // Get slice
+    let slice: &[i32] = &numbers[1..3];
+    println!("Slice: {:?}", slice);
+
+}
---
+++ print.rs
@@ -0,0 +1,24 @@
+pub fn run() {
+
+    // Simple print
+    println!("Hello from the print.rs file");
+
+    // Basic formating
+    println!("{} if from {}", "Brad", "Mass");
+
+    // Positional arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "code");
+
+    // Named arguments
+    println!("{name} likes to play {activity}", name="John", activity="Baseball");
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+
+    // Basic math
+    println!("10 + 10 = {}", 10 + 10);
+
+}
